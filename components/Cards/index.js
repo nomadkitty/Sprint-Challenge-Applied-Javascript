8e55a3be228df5d08940320490e37409e635d9f0
Jojo Zhang <jojozhang0327@gmail.com>
2019-08-09 12:43:35 -0400

    finish MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,52 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container')
+const newArr = []
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res =>{
+        console.log(res);
+        console.log(res.data.articles);
+        const objValueArr = Object.values(res.data.articles)
+       objValueArr.map(subarr =>{
+            subarr.forEach(item => {
+                newArr.push(item);
+            })
+        })
+        console.log(newArr);
+        newArr.forEach(item =>{
+            cardsContainer.appendChild(card(item));
+        })
+    })
+
+function card(obj) {
+    // create elements
+    const card = document.createElement('div');
+    const headL = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const auName = document.createElement('span');
+
+    // set class to element
+    card.classList.add('card');
+    headL.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    // set content to element
+    headL.textContent = obj.headline;
+    img.src = obj.authorPhoto;
+    auName.textContent = obj.authorName;
+
+    // set structure
+    card.appendChild(headL);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(auName);
+    imgContainer.appendChild(img);
+
+    return card
+}
